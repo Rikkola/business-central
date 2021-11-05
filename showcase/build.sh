9c78a59f63d3467840abae91dd8fc0f83771862a
rikkola <toni@rikkola.net>
2021-11-05 17:59:56 +0200

    additional changes
    (cherry picked from commit d48504387d50c7b71973711e1e994e9a48ee8d6c)

--- showcase/build.sh
+++ showcase/build.sh
@@ -11,4 +11,5 @@ IMAGE_TAG="7.61.0.Final"
 # Build the container image.
 echo "Building the Docker container for $IMAGE_NAME:$IMAGE_TAG.."
 docker build --rm -t $IMAGE_NAME:$IMAGE_TAG .
+
 echo "Build done"
